a7b1a559f791c687911ad1cc5c1fdfe5ffd70171
akmalgt <deregen882@gmail.com>
2025-10-31 20:25:43 +0700

    Pertemuan 5, Pake Construct

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,18 +3,27 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+
+    public function __construct($iniJari = 1) {
+        $this->jari = $iniJari;
+    }
+
     public function tampil($nama = 'roda') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
         echo "Lingkaran {$nama} ini hasilnya adalah {$rumus} ";
     }
 
     public static function testing() {
-        echo "Ini testing static";
+        echo "Ini testing static ";
+    }
+
+    public function __destruct() {
+        echo "Malas Kali ";
     }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 10;
+$lingkaran = new LuasLingkaran(10);
+// $lingkaran->jari = 10;
 $lingkaran->tampil("roda");
 
 LuasLingkaran::testing();
