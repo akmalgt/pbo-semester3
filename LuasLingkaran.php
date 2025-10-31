3df76dc77ce6d7528273f4b6125c9d2e8839498c
akmalgt <deregen882@gmail.com>
2025-10-31 20:16:41 +0700

    Pertemuan 4

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,6 +3,14 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+    public function tampil($nama = 'roda') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} ini hasilnya adalah {$rumus} ";
+    }
+
+    public static function testing() {
+        echo "Ini testing static";
+    }
 }
 
 $lingkaran = new LuasLingkaran();
