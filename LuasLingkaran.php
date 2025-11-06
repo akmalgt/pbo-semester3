e81100acd2a0adbcaba08a89c94e6d4e39d5a2bb
akmalgt <97155720+akmalgt@users.noreply.github.com>
2025-11-06 22:57:35 +0700

    Pertemuan 3

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -13,4 +13,4 @@ $lingkaran->jari = 10; //input nilai jari-jari
 
 $rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
 
-echo "Hasil nya adalah: " . $rumus
+echo "Hasil nya adalah: " . $rumus;
