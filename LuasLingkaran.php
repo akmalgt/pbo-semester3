687fb07f205b1127c732c3f5e1abe6f22ec3dccc
akmalgt <97155720+akmalgt@users.noreply.github.com>
2025-11-06 22:59:23 +0700

    Pake Method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,13 +3,15 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
-    public function tampil($nama = 'roda') {
+    
+    public function tampil($nama = 'ban') { 
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} ini hasilnya adalah {$rumus} ";
+        echo "Lingkaran {$nama} ini hasil nya adalah: {$rumus}";
     }
 
     public static function testing() {
-        echo "Ini testing static";
+        echo "<br/>";
+        echo "ini testing static";
     }
 }
 
